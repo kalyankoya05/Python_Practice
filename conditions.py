a7ea95467ae69b96e56446a218d8144c6f8390ac
kalyankoya05 <140375090+kalyankoya05@users.noreply.github.com>
2025-09-03 11:26:22 +0000

    a

--- conditions.py
+++ conditions.py
@@ -5,12 +5,12 @@ emp1 = {"name":"Kalyan","Skill":"DevOps","ID":1234}
 emp2 = {"Name":"Nani","Skill":"Python","Id":2345}
 
 usr_data = input("Enter your skill: ")
-
+usr_data_int = int(usr_data)
 if usr_data in DevOps:
     print(f"Your skill {usr_data} in DevOps team")
 elif usr_data in Dev:
     print(f"Your Skill {usr_data} in Dev team")
-elif usr_data in emp1.values() or usr_data in emp2.values():
+elif usr_data in emp1.values() or usr_data in emp2.values() or usr_data_int in emp1.values() or usr_data_int in emp2.values():
     print(f"your data {usr_data} in employee details")
 else:
     print("Not found")
---
+++ loops.py
@@ -0,0 +1,18 @@
+# For
+
+# Planet = ["Earth","Sun"]
+
+# for i in Planet:
+#     print(i)
+
+# nums = "12343321"
+
+# for i in nums:
+#     print("iteration" , i)
+
+x = 1
+
+while x < 5:
+    print(x)
+    x+=1
+
